1e581e536f21c7ceaf099d143382c5f204accfa8
rkannagala <93792117+rkannagala@users.noreply.github.com>
2021-12-02 16:46:40 -0600

    Update provider.tf

--- Testlab/provider.tf
+++ Testlab/provider.tf
@@ -1,29 +1,20 @@
-#Providers
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
-      # Specify what version of the provider we are going to utilise
       source  = "hashicorp/azurerm"
-      version = ">= 2.79.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
+      version = "=2.46.0"
     }
   }
 }
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
-    }
-  }
+  features {}
 
-
-provider "azuread" {
-  client_id     = "a5ee396b-ae09-4239-8c96-dd11104a2e6e"
-  client_secret = "g6eMFQvM6ESjve96YeD7riZH0_REgETj83"
-  tenant_id     = "6e8ee983-62bd-4020-bfd2-bb3023312bcc"
+  subscription_id = "d8e73157-6307-42c9-bf80-a9464c8d17bd"
+  client_id       = "a5ee396b-ae09-4239-8c96-dd11104a2e6e"
+  client_secret   = "g6eMFQvM6ESjve96YeD7riZH0_REgETj83"
+  tenant_id       = "g6eMFQvM6ESjve96YeD7riZH0_REgETj83"
 }
-}
-
