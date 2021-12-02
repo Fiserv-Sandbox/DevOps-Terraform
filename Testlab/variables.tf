60bda2297ca3d578a474972b031f6c19a4438eb2
rkannagala <93792117+rkannagala@users.noreply.github.com>
2021-12-02 15:15:56 -0600

    Create variables.tf

---
+++ Testlab/variables.tf
@@ -0,0 +1,16 @@
+variable "environment_tag" {
+  type        = string
+  description = "Environment tag value"
+}
+variable "azure-rg-1" {
+  type        = string
+  description = "devops-resource -1"
+}
+variable "azure-rg-2" {
+  description = "resource group 2"
+  type        = string
+}
+variable "loc1" {
+  description = "The location for this Lab environment"
+  type        = string
+}
