c8ee5b02e574440a19073a1a5d6efe5831d53742
rkannagala <93792117+rkannagala@users.noreply.github.com>
2021-12-02 16:10:22 -0600

    Update provider.tf

--- Testlab/provider.tf
+++ Testlab/provider.tf
@@ -18,4 +18,12 @@ provider "azurerm" {
       purge_soft_delete_on_destroy = true
     }
   }
+
+
+provider "azuread" {
+  client_id     = "a5ee396b-ae09-4239-8c96-dd11104a2e6e"
+  client_secret = "g6eMFQvM6ESjve96YeD7riZH0_REgETj83"
+  tenant_id     = "6e8ee983-62bd-4020-bfd2-bb3023312bcc"
 }
+}
+
