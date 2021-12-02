700b19a6634d904d2658f8a199d5bd50c7650ab6
rkannagala <93792117+rkannagala@users.noreply.github.com>
2021-12-02 15:10:23 -0600

    Create azure-deploy.tf

---
+++ Testlab/azure-deploy.tf
@@ -0,0 +1,9 @@
+#Resource Groups
+resource "azurerm_resource_group" "rg1" {
+  name     = var.azure-rg-1
+  location = var.loc1
+  tags = {
+    Environment = var.environment_tag
+    Function    = "baselabv1-resourcegroups"
+  }
+}
